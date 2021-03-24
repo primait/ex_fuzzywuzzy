f69bbe909427daaa55555c40759424e4831afc93
Carlo Suriano <carlo.suriano@prima.it>
2021-03-24 13:19:56 +0100

    [PRIMA-8591]: Config style

--- config/config.exs
+++ config/config.exs
@@ -2,10 +2,9 @@ import Config
 
 config :logger, :console, colors: [enabled: false]
 
-config :ex_fuzzywuzzy, :similarity_fn, &ExFuzzywuzzy.Similarity.Levenshtein.calculate/2
-
-config :ex_fuzzywuzzy, :case_sensitive, false
-
-config :ex_fuzzywuzzy, :precision, 2
+config :ex_fuzzywuzzy,
+  similarity_fn: &ExFuzzywuzzy.Similarity.Levenshtein.calculate/2,
+  case_sensitive: false,
+  precision: 0
 
 import_config "#{config_env()}.exs"
--- config/test.exs
+++ config/test.exs
@@ -1,1 +1,3 @@
 import Config
+
+config :ex_fuzzywuzzy, :precision, 2
