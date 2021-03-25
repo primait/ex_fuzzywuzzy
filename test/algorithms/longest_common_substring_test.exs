626ce204a1edb6111e272334f08d60089ac0af36
Carlo Suriano <carlo.suriano@prima.it>
2021-03-25 16:45:15 +0100

    [PRIMA-8591]: Rewrite LCS basedd on Rosetta Code algorithm

--- test/algorithms/longest_common_substring_test.exs
+++ test/algorithms/longest_common_substring_test.exs
@@ -4,42 +4,42 @@ defmodule ExFuzzywuzzy.Test do
   alias ExFuzzywuzzy.Algorithms.LongestCommonSubstring
 
   test "lcs" do
-    assert LCS.lcs("aaaaaa", "aaaaaa") == %LongestCommonSubstring{
+    assert LongestCommonSubstring.lcs("aaaaaa", "aaaaaa") == %LongestCommonSubstring{
              substring: "aaaaaa",
              left_starting_index: 0,
              right_starting_index: 0,
              length: 6
            }
 
-    assert LCS.lcs("XXXaaaaaaXXXX", "YYaaaaaaYYYYY") == %LongestCommonSubstring{
+    assert LongestCommonSubstring.lcs("XXXaaaaaaXXXX", "YYaaaaaaYYYYY") == %LongestCommonSubstring{
              substring: "aaaaaa",
              left_starting_index: 3,
              right_starting_index: 2,
              length: 6
            }
 
-    assert LCS.lcs("aabc", "dc") == %LongestCommonSubstring{
+    assert LongestCommonSubstring.lcs("aabc", "dc") == %LongestCommonSubstring{
              substring: "c",
              left_starting_index: 3,
              right_starting_index: 1,
              length: 1
            }
 
-    assert LCS.lcs("XXXaaaaaaXXXXaaaaaa", "YYaaaaaaYYYYYaaaaaa") == %LongestCommonSubstring{
+    assert LongestCommonSubstring.lcs("XXXaaaaaaXXXXaaaaaa", "YYaaaaaaYYYYYaaaaaa") == %LongestCommonSubstring{
              substring: "aaaaaa",
              left_starting_index: 3,
              right_starting_index: 2,
              length: 6
            }
 
-    assert LCS.lcs("XaaXaaa", "YaaYaaa") == %LongestCommonSubstring{
+    assert LongestCommonSubstring.lcs("XaaXaaa", "YaaYaaa") == %LongestCommonSubstring{
              substring: "aaa",
              left_starting_index: 4,
              right_starting_index: 4,
              length: 3
            }
 
-    assert LCS.lcs("XXXaaaaaaXXXXaaaaaaa", "YYaaaaaaYYYYYaaaaaaa") == %LongestCommonSubstring{
+    assert LongestCommonSubstring.lcs("XXXaaaaaaXXXXaaaaaaa", "YYaaaaaaYYYYYaaaaaaa") == %LongestCommonSubstring{
              substring: "aaaaaaa",
              left_starting_index: 13,
              right_starting_index: 13,
